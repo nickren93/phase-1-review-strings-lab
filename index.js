a86e8e5efa3742eceed425f61a89d4d5d951a9cc
Sitong <nickren93@gmail.com>
2024-10-24 13:33:23 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'Nick'
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase()
+let shortGreeting = `Welcome, ${currentUser[0]}!`
